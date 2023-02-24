7fd548de3d8ea44359256f33b5ee2214657761d6
notorious <huynhtrieutrivi@gmail.com>
2023-02-23 20:36:15 -0800

    Initial commit

---
+++ Projects/HelloWorld/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        for (String ele : args) {
+            System.out.println(ele);
+        }
+    }
+}
